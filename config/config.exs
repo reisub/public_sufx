38a75d3357058f3779b9bd2307b7337bd2cd0a17
Dino Kovač <dino@latinum.xyz>
2025-04-20 20:11:10 +0200

    Change :download_data_on_compile default to false

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,6 @@
 import Config
 
-config :public_sufx, download_data_on_compile: true
+config :public_sufx, download_data_on_compile: false
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
